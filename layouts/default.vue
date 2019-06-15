7c62a75ad0ada2ddcebb5ff66a1d26835078a2bd
Xuan Do <doxuancntp@gmail.com>
2019-06-15 17:44:59 -0400

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <navbar></navbar>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+html {
+  font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+
+</style>
